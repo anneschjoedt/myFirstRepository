8a5140b61787a50c858ea2617e0140a306c6c5f9
Anne Schjødt <anne.schjoedt@gmail.com>
2019-11-10 21:24:35 +0100

    use linkedlist

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,25 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(new File("students.txt"));
+		LinkedList<String> names = new LinkedList();
+		names.add("Caleb");
+		names.add("Sue");
+		names.add("Sally");
 
-		List<String> students = new ArrayList<>();
+		System.out.println(names.remove());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 
-		while(in.hasNextLine()) {
-			students.add(in.nextLine()); 
-			}
-
-		// goes through each student in the txt file
-		// students.get(i) passes in the index i
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
-		}
-		in.close();
+		
 	}
 }
