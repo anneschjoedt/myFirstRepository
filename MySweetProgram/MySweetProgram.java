f4274f35eef76055cb888b91e74077080535bc67
Anne Schjødt <anne.schjoedt@gmail.com>
2019-11-10 19:22:35 +0100

    initial commit

--- MySweetProgram/MySweetProgram.java
+++ MySweetProgram/MySweetProgram.java
@@ -2,26 +2,11 @@ import java.util.Scanner;
 
 public class MySweetProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Enter your name");
+	public static void main(String[] args){  	
+		System.out.println("Hello Word");
 
-        Scanner in = new Scanner(System.in);
-        //String s = in.nextLine();
-
-        // int i = 0; 
-        // while(i<10) {
-        //     System.out.println("i is:" + i);
-        //     i++;
-        // }
-
-
-        int i = 0; //initialization
-
-        do {
-            System.out.println("i is: " + i);
-            i++;
-        } while(i<10);
-            
-        in.close();
-    }
-}
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		System.out.println(s);
+		}
+	}
