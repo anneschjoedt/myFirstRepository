0f922b58927130630e245e9535f48aa15ac455a9
Anne Schjødt <anne.schjoedt@gmail.com>
2019-11-11 10:41:37 +0100

    iterators

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,21 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		LinkedList<String> names = new LinkedList<>();
-		names.push("Caleb");
-		names.push("Sue");
-		names.push("Sally");
+		names.push("Anne");
+		names.push("Astrid");
+		names.push("Morten");
+		
+		Iterator<String> it = names.iterator();
+		it.next();
+		it.next();
 
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-		System.out.println(names.remove());
-
+		for(String s: names) {
+			System.out.println(s);
+		}
 		
 	}
 }
